ac08cbfa28adc74d4eafe5ceb54de194a97035ff
Henrik Panhans <henrik@panhans.dev>
2021-06-01 12:14:45 +0200

    Moving to typescript

---
+++ src/filteredWhere.ts
@@ -0,0 +1,29 @@
+import "./blank"
+
+declare global {
+    interface Array<T> {
+        /**
+         * Returns a new array only containing the items that match the predicate
+         * @param predicate the predicate used for matching elements
+         */
+        filteredWhere(predicate: (element: T) => boolean): Array<T>
+    }
+}
+
+if (Array.prototype.filteredWhere === undefined) {
+    Array.prototype.filteredWhere = function <T>(
+        this: T[],
+        predicate: (element: T) => boolean
+    ) {
+        if (!(typeof predicate === "function")) {
+            throw "Predicate is not a function"
+        }
+        let filteredArray: Array<T> = []
+        for (let item of this) {
+            if (predicate(item)) {
+                filteredArray.push(item)
+            }
+        }
+        return filteredArray
+    }
+}
---
+++ src/firstIndex.ts
@@ -0,0 +1,31 @@
+import "./blank"
+
+declare global {
+    interface Array<T> {
+        /**
+         * Iterates over the array and finds the index of the first element that matches the predicate
+         * @param predicate the predicate used for matching elements
+         */
+        firstIndex(predicate: (element: T) => boolean): number | null
+    }
+}
+
+if (Array.prototype.firstIndex === undefined) {
+    Array.prototype.firstIndex = function <T>(
+        this: T[],
+        predicate: (element: T) => boolean
+    ) {
+        if (!(typeof predicate === "function")) {
+            throw "Passed in predicate is not a function"
+        }
+        if (!this.length) {
+            return null
+        }
+        for (let i = 0; i < this.length; i++) {
+            if (predicate(this[i])) {
+                return i
+            }
+        }
+        return null
+    }
+}
